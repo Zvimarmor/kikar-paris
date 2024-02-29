8b2044f8be37cbc88774a0644f1d6af719603022
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-02-29 13:02:45 +0200

    dijkestra

--- kikar-kruskal.py
+++ kikar-kruskal.py
@@ -1,99 +1,60 @@
-
-
-class Graph:
+class Graph():
+ 
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = []
-
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
-
-    def find(self, parent, i):
-        if parent[i] == i:
-            return i
-        return self.find(parent, parent[i])
-
-    def union(self, parent, rank, x, y):
-        xroot = self.find(parent, x)
-        yroot = self.find(parent, y)
-
-        if rank[xroot] < rank[yroot]:
-            parent[xroot] = yroot
-        elif rank[xroot] > rank[yroot]:
-            parent[yroot] = xroot
-        else:
-            parent[yroot] = xroot
-            rank[xroot] += 1
-
-
-    def kruskal(self):
-        result = []
-        i, e = 0, 0
-        self.graph = sorted(self.graph, key=lambda item: item[2])
-        parent = []
-        rank = []
-
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+ 
+    def printSolution(self, dist):
+        print("Vertex \t Distance from Source")
         for node in range(self.V):
-            parent.append(node)
-            rank.append(0)
-
-        while e < self.V - 1:
-            u, v, w = self.graph[i]
-            i += 1
-            x = self.find(parent, u)
-            y = self.find(parent, v)
-
-            if x != y:
-                e += 1
-                result.append([u, v, w])
-                self.union(parent, rank, x, y)
-
-        #print("Edges in the constructed MST:")
-        #for u, v, weight in result:
-        #    print("%d -- %d == %d" % (u, v, weight))
-
-        return result
-
-    def v_to_v_quick(self, v1, v2):
-        MST = self.kruskal()
-        answer = 0
-        shortest_way = []
-
-        for e in MST:
-            if v1 == e[0] and v2 == e[1]:
-                answer += e[2]
-                shortest_way.append(e)
-                v1 = e[1]
-
-            elif v1 == e[0] and v2 != e[1]:
-                self.v_to_v_quick(v1 = e[1], v2= v2)
-
-        print("Edges in the constructed MST:")
-        for e in shortest_way:
-            print("%d to %d (%d minutes)" % (e[0], e[1], e[2]))
-        print("the way will take %d minutes" % answer)
-
-
-        
-
-
-        
-
-        
-
-
-g = Graph(4)
-g.add_edge(0, 1, 10)
-g.add_edge(0, 2, 6)
-g.add_edge(0, 3, 5)
-g.add_edge(1, 3, 15)
-g.add_edge(2, 3, 4)
-g.kruskal()
-g.v_to_v_quick(0, 1)
-# Output:
-# Edges in the constructed MST:
-# 2 -- 3 == 4
-# 0 -- 3 == 5
-# 0 -- 1 == 10
-# Time complexity: O(E log E) or O(E log V)
-# Space complexity: O(E + V)
+            print(node, "\t\t", dist[node])
+ 
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
+ 
+        # Initialize minimum distance for next node
+        min = 1e7
+ 
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+ 
+        return min_index
+ 
+    # Function that implements Dijkstra's single source
+    # shortest path algorithm for a graph represented
+    # using adjacency matrix representation
+    def dijkstra(self, src):
+ 
+        dist = [1e7] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+ 
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minDistance(dist, sptSet)
+ 
+            # Put the minimum distance vertex in the
+            # shortest path tree
+            sptSet[u] = True
+ 
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for v in range(self.V):
+                if (self.graph[u][v] > 0 and
+                   sptSet[v] == False and
+                   dist[v] > dist[u] + self.graph[u][v]):
+                    dist[v] = dist[u] + self.graph[u][v]
+ 
+        self.printSolution(dist)
