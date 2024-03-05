53519ace7516f92684d9b86a99051be0dff7e3f1
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-03-05 18:41:45 +0200

    including timer

--- kikar_dijkstra.py
+++ kikar_dijkstra.py
@@ -35,64 +35,74 @@ class Graph:
                     heapq.heappush(vertices, (distance, neighbor))
                     #pushes the neighbor into the "distance" heap
 
-        path = set()
+        path = []
         current_vertex = end
 
         if current_vertex not in previous_vertices:
             return -1, []
 
         while current_vertex is not None:
-            path.add(current_vertex)
+            path.insert(0, current_vertex)
             current_vertex = previous_vertices[current_vertex]
 
         return distances[end], path
 
 #check what is the shortest path from the start to the end
-# Graph 1
-g1 = Graph(6)
-g1.add_edge(0, 1, 5)
-g1.add_edge(0, 2, 10)
-g1.add_edge(1, 2, 3)
-g1.add_edge(1, 3, 8)
-g1.add_edge(2, 3, 1)
-g1.add_edge(3, 4, 2)
-print(g1.dijkstra(0, 5))  # Expected: (16, [0, 2, 3, 4, 5])
+# # Graph 1
+# g1 = Graph(6)
+# g1.add_edge(0, 1, 5)
+# g1.add_edge(0, 2, 10)
+# g1.add_edge(1, 2, 3)
+# g1.add_edge(1, 3, 8)
+# g1.add_edge(2, 3, 1)
+# g1.add_edge(3, 4, 2)
+# print(g1.dijkstra(0, 5))  # Expected: (16, [0, 2, 3, 4, 5])
 
-# Graph 2
-g2 = Graph(8)
-g2.add_edge(0, 1, 2)
-g2.add_edge(0, 2, 5)
-g2.add_edge(1, 2, 2)
-g2.add_edge(1, 3, 1)
-g2.add_edge(2, 4, 6)
-g2.add_edge(3, 4, 3)
-g2.add_edge(3, 5, 8)
-g2.add_edge(4, 6, 7)
-g2.add_edge(5, 6, 4)
-g2.add_edge(5, 7, 2)
-g2.add_edge(6, 7, 5)
-print(g2.dijkstra(0, 7))  # Expected: (15, [0, 2, 4, 6, 7])
+# # Graph 2
+# g2 = Graph(8)
+# g2.add_edge(0, 1, 2)
+# g2.add_edge(0, 2, 5)
+# g2.add_edge(1, 2, 2)
+# g2.add_edge(1, 3, 1)
+# g2.add_edge(2, 4, 6)
+# g2.add_edge(3, 4, 3)
+# g2.add_edge(3, 5, 8)
+# g2.add_edge(4, 6, 7)
+# g2.add_edge(5, 6, 4)
+# g2.add_edge(5, 7, 2)
+# g2.add_edge(6, 7, 5)
+# print(g2.dijkstra(0, 7))  # Expected: (15, [0, 2, 4, 6, 7])
 
-# Graph 3
-g3 = Graph(5)
-g3.add_edge(0, 1, 3)
-g3.add_edge(0, 2, 2)
-g3.add_edge(1, 2, 1)
-g3.add_edge(1, 3, 5)
-g3.add_edge(2, 3, 2)
-g3.add_edge(2, 4, 4)
-g3.add_edge(3, 4, 1)
-print(g3.dijkstra(0, 4))  # Expected: (6, [0, 2, 4])
+# # Graph 3
+# g3 = Graph(5)
+# g3.add_edge(0, 1, 3)
+# g3.add_edge(0, 2, 2)
+# g3.add_edge(1, 2, 1)
+# g3.add_edge(1, 3, 5)
+# g3.add_edge(2, 3, 2)
+# g3.add_edge(2, 4, 4)
+# g3.add_edge(3, 4, 1)
+# print(g3.dijkstra(0, 4))  # Expected: (6, [0, 2, 4])
 
-# Graph 4
-g4 = Graph(7)
-g4.add_edge(0, 1, 2)
-g4.add_edge(0, 2, 4)
-g4.add_edge(1, 2, 1)
-g4.add_edge(1, 3, 7)
-g4.add_edge(2, 4, 3)
-g4.add_edge(3, 4, 1)
-g4.add_edge(3, 5, 5)
-g4.add_edge(4, 6, 2)
-g4.add_edge(5, 6, 10)
-print(g4.dijkstra(0, 6))  # Expected: (8, [0, 2, 4, 6])
+# # Graph 4
+# g4 = Graph(7)
+# g4.add_edge(0, 1, 2)
+# g4.add_edge(0, 2, 4)
+# g4.add_edge(1, 2, 1)
+# g4.add_edge(1, 3, 7)
+# g4.add_edge(2, 4, 3)
+# g4.add_edge(3, 4, 1)
+# g4.add_edge(3, 5, 5)
+# g4.add_edge(4, 6, 2)
+# g4.add_edge(5, 6, 10)
+# print(g4.dijkstra(0, 6))  # Expected: (8, [0, 2, 4, 6])
+
+# kikar_paris = Graph(6)
+# kikar_paris.add_edge(0, 1, 0)
+# kikar_paris.add_edge(0, 2, 20)
+# kikar_paris.add_edge(1, 3, 14)
+# kikar_paris.add_edge(2, 4, 19)
+# kikar_paris.add_edge(3, 5, 12)
+# kikar_paris.add_edge(4, 5, 0)
+
+# print(kikar_paris.dijkstra(0,1)) # Expected: (33, [2, 4, 5, 3]) 
---
+++ kikar_time.py
@@ -0,0 +1,14 @@
+import time
+import subprocess
+import sys
+from kikar_dijkstra import Graph
+
+def run_program():
+    # Your program logic goes here
+    print("Running the program...")
+
+while True:
+    run_program()
+
+    # Sleep for one second before running the program again
+    time.sleep(1)
