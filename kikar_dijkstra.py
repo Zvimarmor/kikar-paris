ecaa7fe49864eacbaa9d8d3eb8492c5cf07a931e
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-03-07 12:02:17 +0200

    addinng the res light and green light times and the status. need to fix the run program func

--- kikar_dijkstra.py
+++ kikar_dijkstra.py
@@ -5,21 +5,21 @@ class Graph:
         self.vertices = set()
         self.edges = {}
 
-    def add_edge(self, u, v, weight):
+    def add_edge(self, u, v, current_time, red_light_time, green_light_time, status): #status is 0 for red and 1 for green
         self.vertices.add(u)
         self.vertices.add(v)
-        self.edges.setdefault(u, []).append((v, weight))
-        self.edges.setdefault(v, []).append((u, weight))
+        self.edges.setdefault(u, []).append((v, current_time, red_light_time, green_light_time, status))
+        self.edges.setdefault(v, []).append((u, current_time, red_light_time, green_light_time, status))
         #this two lines are simetric because the graph isn't directed
 
-    def change_weight(self, u, v, weight):
+    def change_weight(self, u, v, current_time, red_light_time, green_light_time ,status):
         for i, (neighbor, w) in enumerate(self.edges[u]):
             if neighbor == v:
-                self.edges[u][i] = (neighbor, weight)
+                self.edges[u][i] = (neighbor, current_time, red_light_time, green_light_time, status)
                 break
         for i, (neighbor, w) in enumerate(self.edges[v]):
             if neighbor == u:
-                self.edges[v][i] = (neighbor, weight)
+                self.edges[v][i] = (neighbor, current_time, red_light_time, green_light_time, status)
                 break
 
     def dijkstra(self, start, end):
@@ -36,8 +36,8 @@ class Graph:
             if current_distance > distances[current_vertex]:
                 continue
 
-            for neighbor, weight in self.edges.get(current_vertex, []):
-                distance = current_distance + weight
+            for neighbor, current_time, red_light_time, green_light_time, status in self.edges[current_vertex]:
+                distance = current_distance + current_time
 
                 if distance < distances[neighbor]:
                     distances[neighbor] = distance
@@ -57,62 +57,67 @@ class Graph:
 
         return distances[end], path
 
-#check what is the shortest path from the start to the end
-# # Graph 1
-# g1 = Graph(6)
-# g1.add_edge(0, 1, 5)
-# g1.add_edge(0, 2, 10)
-# g1.add_edge(1, 2, 3)
-# g1.add_edge(1, 3, 8)
-# g1.add_edge(2, 3, 1)
-# g1.add_edge(3, 4, 2)
-# print(g1.dijkstra(0, 5))  
+# # Test cases
+# # 1. The graph is a line
+# line = Graph(3)
+# line.add_edge(0, 1, 10, 10, 10, 0)
+# line.add_edge(1, 2, 10, 10, 10, 1)
+# print(line.dijkstra(0, 2))  # Expected: (20, [0, 1, 2])
 
-# # Graph 2
-# g2 = Graph(8)
-# g2.add_edge(0, 1, 2)
-# g2.add_edge(0, 2, 5)
-# g2.add_edge(1, 2, 2)
-# g2.add_edge(1, 3, 1)
-# g2.add_edge(2, 4, 6)
-# g2.add_edge(3, 4, 3)
-# g2.add_edge(3, 5, 8)
-# g2.add_edge(4, 6, 7)
-# g2.add_edge(5, 6, 4)
-# g2.add_edge(5, 7, 2)
-# g2.add_edge(6, 7, 5)
-# print(g2.dijkstra(0, 7))  # Expected: (15, [0, 2, 4, 6, 7])
+# # 2. The graph is a tree
+# tree = Graph(5)
+# tree.add_edge(0, 1, 10, 10, 10, 0)
+# tree.add_edge(0, 2, 10, 10, 10, 1)
+# tree.add_edge(1, 3, 10, 10, 10, 0)
+# tree.add_edge(1, 4, 10, 10, 10, 1)
+# print(tree.dijkstra(0, 4))  # Expected: (20, [0, 1, 4])
 
-# # Graph 3
-# g3 = Graph(5)
-# g3.add_edge(0, 1, 3)
-# g3.add_edge(0, 2, 2)
-# g3.add_edge(1, 2, 1)
-# g3.add_edge(1, 3, 5)
-# g3.add_edge(2, 3, 2)
-# g3.add_edge(2, 4, 4)
-# g3.add_edge(3, 4, 1)
-# print(g3.dijkstra(0, 4))  # Expected: (6, [0, 2, 4])
+# # 3. The graph is a star
+# star = Graph(5)
+# star.add_edge(0, 1, 10, 10, 10, "red")
+# star.add_edge(0, 2, 10, 10, 10, "green")
+# star.add_edge(0, 3, 10, 10, 10, "red")
+# star.add_edge(0, 4, 10, 10, 10, "green")
+# print(star.dijkstra(0, 4))  # Expected: (10, [0, 4])
 
-# # Graph 4
-# g4 = Graph(7)
-# g4.add_edge(0, 1, 2)
-# g4.add_edge(0, 2, 4)
-# g4.add_edge(1, 2, 1)
-# g4.add_edge(1, 3, 7)
-# g4.add_edge(2, 4, 3)
-# g4.add_edge(3, 4, 1)
-# g4.add_edge(3, 5, 5)
-# g4.add_edge(4, 6, 2)
-# g4.add_edge(5, 6, 10)
-# print(g4.dijkstra(0, 6))  # Expected: (8, [0, 2, 4, 6])
+# # 4. The graph is a square
+# square = Graph(4)
+# square.add_edge(0, 1, 10, 10, 10, "red")
+# square.add_edge(1, 2, 10, 10, 10, "green")
+# square.add_edge(2, 3, 10, 10, 10, "red")
+# square.add_edge(3, 0, 10, 10, 10, "green")
+# print(square.dijkstra(0, 2))  # Expected: (20, [0, 1, 2])
+    
+# # Test cases
+# # 5. The graph is a complex graph with a cycle and 20 vertices
+# complex_graph = Graph(20)
+# complex_graph.add_edge(0, 1, 10, 10, 10, "red")
+# complex_graph.add_edge(1, 2, 10, 10, 10, "green")
+# complex_graph.add_edge(2, 3, 10, 10, 10, "red")
+# complex_graph.add_edge(3, 4, 10, 10, 10, "green")
+# complex_graph.add_edge(4, 5, 10, 10, 10, "red")
+# complex_graph.add_edge(5, 6, 10, 10, 10, "green")
+# complex_graph.add_edge(6, 7, 10, 10, 10, "red")
+# complex_graph.add_edge(7, 8, 10, 10, 10, "green")
+# complex_graph.add_edge(8, 9, 10, 10, 10, "red")
+# complex_graph.add_edge(9, 10, 10, 10, 10, "green")
+# complex_graph.add_edge(10, 11, 10, 10, 10, "red")
+# complex_graph.add_edge(11, 12, 10, 10, 10, "green")
+# complex_graph.add_edge(12, 13, 10, 10, 10, "red")
+# complex_graph.add_edge(13, 14, 10, 10, 10, "green")
+# complex_graph.add_edge(14, 15, 10, 10, 10, "red")
+# complex_graph.add_edge(15, 16, 10, 10, 10, "green")
+# complex_graph.add_edge(16, 17, 10, 10, 10, "red")
+# complex_graph.add_edge(17, 18, 10, 10, 10, "green")
+# complex_graph.add_edge(18, 19, 10, 10, 10, "red")
+# complex_graph.add_edge(19, 0, 10, 10, 10, "green")
+# print(complex_graph.dijkstra(0, 19))  # Expected: (10, [0, 1])
 
 # kikar_paris = Graph(6)
-# kikar_paris.add_edge(0, 1, 20)
-# kikar_paris.add_edge(0, 2, 20)
-# kikar_paris.add_edge(1, 3, 14)
-# kikar_paris.add_edge(2, 4, 19)
-# kikar_paris.add_edge(3, 5, 12)
-# kikar_paris.add_edge(4, 5, 0)
-
-# print(kikar_paris.dijkstra(0,5)) # Expected: (33, [2, 4, 5, 3]) 
+# kikar_paris.add_edge(0, 1, 20, 10, 10, "red")
+# kikar_paris.add_edge(0, 2, 60, 10, 10, "green")
+# kikar_paris.add_edge(1, 3, 14, 10, 10, "red")
+# kikar_paris.add_edge(2, 4, 19, 10, 10, "green")
+# kikar_paris.add_edge(3, 5, 12, 10, 10, "red")
+# kikar_paris.add_edge(4, 5, 0, 10, 10, "green")
+# print(kikar_paris.dijkstra(0, 5))  # Expected: (33, [2, 4, 5, 3])
--- kikar_time.py
+++ kikar_time.py
@@ -9,7 +9,8 @@ def build_graph():
     
     # Parse the string representation of edges into a list
     edges_str = sys.argv[2].split( )
-    edges = [(int(edges_str[i]), int(edges_str[i+1]), int(edges_str[i+2])) for i in range(0, len(edges_str), 4)]
+    edges = [(int(edges_str[i]), int(edges_str[i+1]), int(edges_str[i+2],int(edges_str[i+3]), int(edges_str[i+4]), int(edges_str[i+5])))\
+               for i in range(0, len(edges_str), 6)]
 
     for edge in edges:
         junction.add_edge(edge[0], edge[1],edge[2])
@@ -17,13 +18,19 @@ def build_graph():
 
 def run_program(junction):
     for edge in junction.edges:
-        junction.change_weight(edge[0], edge[1], edge[2]-1)
+        if edge[2] <= 0:
+            if edge[4] == 0:
+                junction.change_weight(edge[0], edge[1], edge[3], edge[3], edge[4], 1)
+            if edge[4] == 1:
+                junction.change_weight(edge[0], edge[1], edge[4], edge[3], edge[4], 0)
+            
+        junction.change_weight(edge[0], edge[1], edge[2]-1, edge[3], edge[4], edge[5])
 
     answer = junction.dijkstra(int(sys.argv[3]), int(sys.argv[4]))
     print(answer)
-    
+
+build_graph()
 run_program()
-    
 
 while True:
     run_program()
